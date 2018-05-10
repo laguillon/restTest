93a27e9afff543500cfe62c2e603b31c1a91db55
P. Leonard Aguillon <leo@Leos-MacBook-Pro.local>
2018-05-09 22:16:23 -0500

    add some di

---
+++ src/main/java/hello/Foo.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Foo {
+	
+	public String Print()
+	{
+		return "blah";
+	}
+}
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,8 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,9 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    
+    @Autowired
+    Foo foo;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
@@ -22,5 +27,11 @@ public class GreetingController {
     public Multiplier Multiply(@RequestParam(value="a") int a, @RequestParam(value="b") int b)
     {
     	return new Multiplier(a, b);
+    }
+    
+    @RequestMapping("/foo")
+    public String foo()
+    {
+    	return foo.Print();
     }
 }
